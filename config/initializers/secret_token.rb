16f8d9108ff4f58ca1c33fdb7ee2365d9a086431
Claire King <riotousamusements@gmail.com>
2013-11-07 00:09:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riotousamusements::Application.config.secret_token = '67a0adea3544d742b36ecf485f7881eb499ba5e6a64338c63571d4a8bffa0c9cd8358dcc30ff6744d785f073fe15281b3abe5264bd2410269b0a982c5665fa1f'
